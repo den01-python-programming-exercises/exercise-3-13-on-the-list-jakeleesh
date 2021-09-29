5edc8947ce81fb06ba9026363659e2d4a1edfbb3
Jake Lee <jake.lee@fdmgroup.com>
2021-09-29 16:55:27 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    names = []
+    while True:
+        name = input()
+        if (name == ""):
+            break
+        else:
+            names.append(name)
+    search = input("Search for?")
+    if search in names:
+        print(search + " was found!")
+    else:
+        print(search + " was not found!")
 
 if __name__ == '__main__':
     main()
